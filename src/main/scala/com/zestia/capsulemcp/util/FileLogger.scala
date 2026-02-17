ce97f4d6389f55e5c1e093de02c0ef22c66adea0
Miranda Hawkes <miranda.hawkes@zestia.com>
2026-02-17 17:38:41 +0000

    Write app logs to stderr instead of log file

--- src/main/scala/com/zestia/capsulemcp/util/FileLogger.scala
+++ src/main/scala/com/zestia/capsulemcp/util/FileLogger.scala
@@ -16,30 +16,20 @@
 
 package com.zestia.capsulemcp.util
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths, StandardOpenOption}
 import java.time.Instant
 
 class FileLogger(className: String):
 
-  import FileLogger.*
+  def info(msg: String): Unit = log("info", msg)
 
-  def info(msg: String): Unit = log("INFO", msg)
+  def warn(msg: String): Unit = log("warn", msg)
 
-  def warn(msg: String): Unit = log("WARN", msg)
-
-  def error(msg: String): Unit = log("ERROR", msg)
+  def error(msg: String): Unit = log("error", msg)
 
   private def log(level: String, msg: String): Unit =
     val timestamp = Instant.now().toString // ISO-8601 UTC timestamp
-    val line = s"$timestamp $level [$className] $msg\n"
-    Files.write(logPath, line.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.APPEND)
+    System.err.println(s"$timestamp [$className] [$level] $msg")
 
 object FileLogger:
-  private val logPath = Paths.get("capsule-mcp.log")
-
-  // Initialise log file on startup (create or truncate)
-  def init(): Unit =
-    Files.write(logPath, Array.emptyByteArray, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
-
+  def init(): Unit = ()
   def apply(name: String): FileLogger = new FileLogger(name)
